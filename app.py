1aa0e6b38e38de467e97b9ebabb40afd9f86d856
VaibhaRagavan <vaibha97@gmail.com>
2026-05-28 16:09:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ import main
 st.title("News Bot")
 st.caption("Regional & global news, verified")
 query=st.text_input("Ask about any news topics")
-
+openai_api_key=st.secrets("OPENAI_API_KEY")
+langsmith_api_key=st.secrets("LANGSMITH_API_KEY")
+mcp_url=st.secrets("MCP_URL")
 if st.button("Search"):
     with st.spinner("Fetching news..."):
         result=main.graph(query)

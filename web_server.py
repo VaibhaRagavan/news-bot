10e67391005af4a0409f8253a3a5a1285a01b43d
VaibhaRagavan <vaibha97@gmail.com>
2026-05-28 21:02:17 +0100

    Update web_server.py

--- web_server.py
+++ web_server.py
@@ -114,7 +114,7 @@ def get_old_news(query:str ,days_ago:int)->str:
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    web.run(transport="streamable-http", host="0.0.0.0", port=port)
+    web.run(transport="streamable-http", port=port)
     
    
     
